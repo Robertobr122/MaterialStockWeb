a795ecd48b107add6527c9cf99a2d3df153f244d
Petrus Ermerson <petrus260@hotmail.com>
2024-06-22 23:54:40 -0300

    finalizando lista de cadastro de produtos, solução temporaraia para mover entre os forms em JS

---
+++ JAVASCRIPT/cadastroProdutos.js
@@ -0,0 +1,51 @@
+
+document.getElementById('Form1').addEventListener('click', () => {
+    document.getElementById('informaçõesGerais').style.display = 'block';
+    document.getElementById('valores').style.display = 'none';
+    document.getElementById('Estoque').style.display = 'none';
+    document.getElementById('Fotos').style.display = 'none';
+    document.getElementById('PesoDimencoes').style.display = 'none';
+    document.getElementById('DadosFiscais').style.display = 'none';
+  })
+  document.getElementById('Form2').addEventListener('click', () => {
+    document.getElementById('informaçõesGerais').style.display = 'none';
+    document.getElementById('valores').style.display = 'block';
+    document.getElementById('Estoque').style.display = 'none';
+    document.getElementById('Fotos').style.display = 'none';
+    document.getElementById('PesoDimencoes').style.display = 'none';
+    document.getElementById('DadosFiscais').style.display = 'none';
+  })  
+  document.getElementById('Form3').addEventListener('click', () => {
+    document.getElementById('informaçõesGerais').style.display = 'none';
+    document.getElementById('valores').style.display = 'none';
+    document.getElementById('Estoque').style.display = 'block';
+    document.getElementById('Fotos').style.display = 'none';
+    document.getElementById('PesoDimencoes').style.display = 'none';
+    document.getElementById('DadosFiscais').style.display = 'none';
+  })
+  document.getElementById('Form4').addEventListener('click', () => {
+    document.getElementById('informaçõesGerais').style.display = 'none';
+    document.getElementById('valores').style.display = 'none';
+    document.getElementById('Estoque').style.display = 'none';
+    document.getElementById('Fotos').style.display = 'block';
+    document.getElementById('PesoDimencoes').style.display = 'none';
+    document.getElementById('DadosFiscais').style.display = 'none';
+  })
+  document.getElementById('Form5').addEventListener('click', () => {
+    document.getElementById('informaçõesGerais').style.display = 'none';
+    document.getElementById('valores').style.display = 'none';
+    document.getElementById('Estoque').style.display = 'none';
+    document.getElementById('Fotos').style.display = 'none';
+    document.getElementById('PesoDimencoes').style.display = 'block';
+    document.getElementById('DadosFiscais').style.display = 'none';
+  })
+  document.getElementById('Form6').addEventListener('click', () => {
+    document.getElementById('informaçõesGerais').style.display = 'none';
+    document.getElementById('valores').style.display = 'none';
+    document.getElementById('Estoque').style.display = 'none';
+    document.getElementById('Fotos').style.display = 'none';
+    document.getElementById('PesoDimencoes').style.display = 'none';
+    document.getElementById('DadosFiscais').style.display = 'block';
+})
+  
+  
