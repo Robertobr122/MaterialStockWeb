d589d5f274508c9b6d196b8cb1a9563dcd4d103b
Petrus Ermerson <petrus260@hotmail.com>
2024-06-23 12:54:32 -0300

    removendo mutiplo forms e adicionando a função de cadastar imagem removendo os mutiplos froms e criando somente um from e adicionando a função de imagem para estilo

--- JAVASCRIPT/cadastroProdutos.js
+++ JAVASCRIPT/cadastroProdutos.js
@@ -1,3 +1,4 @@
+//codigo responsavel por monstrar e esconder os forms em cadastroProdutos.html
 document.addEventListener('DOMContentLoaded', function() {
   function toggleDisplay(clickElementId, elementsToHide, elementsToShow) {
 
@@ -18,3 +19,61 @@ document.addEventListener('DOMContentLoaded', function() {
   toggleDisplay('Form5', ['informaçõesGerais', 'valores', 'Estoque', 'Fotos', 'PesoDimencoes', 'DadosFiscais'], ['PesoDimencoes']);
   toggleDisplay('Form6', ['informaçõesGerais', 'valores', 'Estoque', 'Fotos', 'PesoDimencoes', 'DadosFiscais'], ['DadosFiscais']);
 });
+
+//codigo responsavel pela funcionalidade de imagens em cadastroProdutos.html
+const dropArea = document.querySelector(".drag-area"),
+dragText = dropArea.querySelector("header"),
+button = dropArea.querySelector("button"),
+input = dropArea.querySelector("input");
+let file; //variavel global 
+
+button.onclick = ()=>{
+  input.click(); //se o botão foi clickado input também sera clickado
+}
+
+input.addEventListener("change", function(){
+  //recebendo o arquivo do usuário, files[0] significa que só receberemos o primeiro arquivo
+  file = this.files[0];
+  dropArea.classList.add("active");
+  showFile(); //chamando a função
+});
+
+
+//If user Drag File Over DropArea
+dropArea.addEventListener("dragover", (event)=>{
+  event.preventDefault(); //prevenindo default behaviour
+  dropArea.classList.add("active");
+  dragText.textContent = "Solte a imagem Para enviar arquivo";
+});
+
+//If user leave dragged File from DropArea
+dropArea.addEventListener("dragleave", ()=>{
+  dropArea.classList.remove("active");
+  dragText.textContent = "Arraste & solte a imagem aqui para fazer upload";
+});
+
+//If user drop File on DropArea
+dropArea.addEventListener("drop", (event)=>{
+  event.preventDefault(); //prevenindo default behaviour
+  //selecionando o arquivo do usuário [0] significa que se tiver varios arquivos só receberemos o primeiro
+  file = event.dataTransfer.files[0];
+  showFile(); //chamando a função
+});
+
+function showFile(){
+  let fileType = file.type; //pegando o tipo de arquivo selecionado
+  let validExtensions = ["image/jpeg", "image/jpg", "image/png"]; //um array com os formatos de imagem validos
+  if(validExtensions.includes(fileType)){ //se o arquivo do usuário é uma imagem
+    let fileReader = new FileReader(); //creating new FileReader object
+    fileReader.onload = ()=>{
+      let fileURL = fileReader.result; //passing user file source in fileURL variable
+      let imgTag = `<img src="${fileURL}" alt="">`; //criando uma img tag e passando a source da imagem para um src attribute
+      dropArea.innerHTML = imgTag; //adicionando a img tag criada dentro de dropArea container
+    }
+    fileReader.readAsDataURL(file);
+  }else{
+    alert("Isso não é um arquivo de Imagem");
+    dropArea.classList.remove("active");
+    dragText.textContent = "Solte a imagem Para enviar arquivo";
+  }
+}
